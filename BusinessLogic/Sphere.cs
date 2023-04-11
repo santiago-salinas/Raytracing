a68f8c2fbb69d77fde44c77852bb69cb875c80e6
Fede PC <federicorodriguez2002@gmail.com>
2023-04-11 20:55:10 -0300

    [Green] Create Sphere Test

---
+++ BusinessLogic/Sphere.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace BusinessLogic
+{
+    public class Sphere
+    {
+        public Sphere() { }
+
+        private float _radius;
+        public float Radius
+        {
+            get { return _radius; }
+            set { _radius = value; }
+        }
+    }
+}
---
+++ BusinessLogic_Tests/UnitTest1.cs
@@ -0,0 +1,28 @@
+﻿using BusinessLogic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace BusinessLogic_Tests
+{
+    [TestClass]
+    public class SphereTests
+    {
+        private Sphere testSphere;
+        private float testRadius = 5;
+
+        [TestInitialize] 
+        public void Initialize() {
+            testSphere = new Sphere()
+            {
+                Radius = testRadius,
+            };
+        }
+
+        [TestMethod]
+        public void SphereCreatedSuccesfullyTest()
+        {
+            Assert.IsNotNull(testSphere);
+        }
+    }
+    
+}
