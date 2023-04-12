9a26f266b9e8a7f34f589ea4dd8524ea52c2689e
Santiago <santiagosalinasoto@gmail.com>
2023-04-11 21:13:35 -0300

    [Red] RadiusIsOverZeroTest

---
+++ BusinessLogic/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BusinessLogic
+{
+    public class BusinessLogicException : Exception
+    {
+        public BusinessLogicException(string message) : base(message)
+        {
+
+        }
+    }
+}
--- BusinessLogic_Tests/UnitTest1.cs
+++ BusinessLogic_Tests/UnitTest1.cs
@@ -8,20 +8,31 @@ namespace BusinessLogic_Tests
     public class SphereTests
     {
         private Sphere testSphere;
-        private float testRadius = 5;
+        private float testRadius = -5;
 
         [TestInitialize] 
         public void Initialize() {
-            testSphere = new Sphere()
-            {
-                Radius = testRadius,
-            };
+            
         }
 
         [TestMethod]
         public void SphereCreatedSuccesfullyTest()
         {
+            testSphere = new Sphere()
+            {
+                Radius = testRadius,
+            };
             Assert.IsNotNull(testSphere);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(BusinessLogicException), "Radius must be a value over zero >0")]
+        public void RadiusIsOverZeroTest()
+        {
+            testSphere = new Sphere()
+            {
+                Radius = testRadius,
+            };
         }
     }
     
