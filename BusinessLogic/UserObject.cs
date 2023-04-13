6b1fed75e824ad9802de317a33ca38f4de89f9d9
Santiago <santiagosalinasoto@gmail.com>
2023-04-13 17:02:24 -0300

    [Green] UserObjectTests for Name

--- BusinessLogic/UserObject.cs
+++ BusinessLogic/UserObject.cs
@@ -1,4 +1,6 @@
-﻿namespace BusinessLogic
+﻿using System;
+
+namespace BusinessLogic
 {
     public class UserObject
     {
@@ -6,6 +8,33 @@
         {
         }
 
-        public string Name { get; set; }
+        private string _name;
+        
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                value = value.Trim();
+
+                CheckIfStringNull(value);
+
+                _name = value;
+            }
+        }
+
+        private void CheckIfStringNull(string value)
+        {
+            if (String.IsNullOrEmpty(value))
+            {
+                throw new ArgumentNullException("Name cant be null");
+            }
+        }
+
+
+        public bool Equals(Sphere other)
+        {
+            return this.Name == other.Name;
+        }
     }
 }
