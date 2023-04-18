c0cce0a7cbbe65cfc682142a13c311059a10674f
Santiago <santiagosalinasoto@gmail.com>
2023-04-18 20:25:59 -0300

    [Green] SceneNameTests

--- BusinessLogic/Objects/Scene.cs
+++ BusinessLogic/Objects/Scene.cs
@@ -1,18 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Xml.Linq;
 
 namespace BusinessLogic
 {
     public class Scene
     {
         private List<Model> _positionedModellList;
+        private string _name;
 
         public Scene()
         {
             _positionedModellList = new List<Model>();
         }
 
-        public string Name { get; set; }
+        public string Name {
+            get { return _name; }
+            set
+            {
+                value = value.Trim();
+                CheckIfStringNull(value);
+                _name = value;
+            }
+        }
+
+        private void CheckIfStringNull(string value)
+        {
+            if (String.IsNullOrEmpty(value))
+            {
+                throw new ArgumentNullException("Name cant be null");
+            }
+        }
+
         public Vector LookFrom { get; set; }
         public Vector LookAt { get; set; }
 
