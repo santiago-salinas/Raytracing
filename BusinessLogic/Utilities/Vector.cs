403d1bf89c31571d55a3bba87b8f804158873696
Fede PC <federicorodriguez2002@gmail.com>
2023-04-25 20:50:50 -0300

    [Green] Vector and Ray unit tests

--- BusinessLogic/Utilities/Vector.cs
+++ BusinessLogic/Utilities/Vector.cs
@@ -29,9 +29,10 @@ namespace BusinessLogic
             ThrdValue = z;
         }
 
-        public double FstValue {
+        public double FstValue
+        {
             get { return _fstValue; }
-            set { _fstValue = value; } 
+            set { _fstValue = value; }
         }
         public double SndValue
         {
@@ -46,14 +47,89 @@ namespace BusinessLogic
 
         public override bool Equals(object other)
         {
-            bool evalFst = this.FstValue == ((Vector)other).FstValue;
-            bool evalSnd = this.SndValue == ((Vector)other).SndValue;
-            bool evalThrd = this.ThrdValue == ((Vector)other).ThrdValue;
+            Vector otherVector = (Vector)other;
+            const double tolerance = 0.0001;
+
+            bool evalFst = Math.Abs(FstValue - otherVector.FstValue) < tolerance;
+            bool evalSnd = Math.Abs(SndValue - otherVector.SndValue) < tolerance;
+            bool evalThrd = Math.Abs(ThrdValue - otherVector.ThrdValue) < tolerance;
 
             return evalFst && evalSnd && evalThrd;
         }
 
+        public Vector Add(Vector other)
+        {
+            return new Vector(_fstValue + other.FstValue, _sndValue + other.SndValue, _thrdValue + other.ThrdValue);
+        }
 
+        public Vector Subtract(Vector other)
+        {
+            return new Vector(_fstValue - other.FstValue, _sndValue - other.SndValue, _thrdValue - other.ThrdValue);
+        }
 
+        public Vector Multiply(double value)
+        {
+            return new Vector(_fstValue * value, _sndValue * value, _thrdValue * value);
+        }
+
+        public Vector Divide(double value)
+        {
+            return new Vector(_fstValue / value, _sndValue / value, _thrdValue / value);
+        }
+
+        public void AddTo(Vector other)
+        {
+            _fstValue += other.FstValue;
+            _sndValue += other.SndValue;
+            _thrdValue += other.ThrdValue;
+        }
+
+        public void SubtractFrom(Vector other)
+        {
+            _fstValue -= other.FstValue;
+            _sndValue -= other.SndValue;
+            _thrdValue -= other.ThrdValue;
+        }
+
+        public void ScaleUpBy(int iCount)
+        {
+            _fstValue *= iCount;
+            _sndValue *= iCount;
+            _thrdValue *= iCount;
+        }
+
+        public void ScaleDownBy(int iCount)
+        {
+            _fstValue /= iCount;
+            _sndValue /= iCount;
+            _thrdValue /= iCount;
+        }
+
+        public double SquaredLength()
+        {
+            return _fstValue * _fstValue + _sndValue * _sndValue + _thrdValue * _thrdValue;
+        }
+
+        public double Length()
+        {
+            return Math.Sqrt(SquaredLength());
+        }
+        public Vector GetUnit()
+        {
+            return Divide(Length());
+        }
+
+        public double Dot(Vector other)
+        {
+            return _fstValue * other.FstValue + _sndValue * other.SndValue + _thrdValue * other.ThrdValue;
+        }
+
+        public Vector Cross(Vector other)
+        {
+            double x = _sndValue * other.ThrdValue - _thrdValue * other.SndValue;
+            double y = _thrdValue * other.FstValue - _fstValue * other.ThrdValue;
+            double z = _fstValue * other.SndValue - _sndValue * other.FstValue;
+            return new Vector(x, y, z);
+        }
     }
 }
