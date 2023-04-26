a3d5892015a7d00a52c3d5a1d890c012ffcdd196
FACULTADES\alumnoFI <vdsv@ghfg>
2023-04-26 09:56:35 -0300

    [Red] Rendering unit test

--- BusinessLogic/Utilities/Vector.cs
+++ BusinessLogic/Utilities/Vector.cs
@@ -9,40 +9,40 @@ namespace BusinessLogic
 {
     public class Vector
     {
-        private double _fstValue;
-        private double _sndValue;
-        private double _thrdValue;
+        private double _firstValue;
+        private double _secondValue;
+        private double _thirdValue;
 
         public Vector()
         { }
 
         public Vector(double x, double y, double z)
         {
-            FstValue = x;
-            SndValue = y;
-            ThrdValue = z;
+            FirstValue = x;
+            SecondValue = y;
+            ThirdValue = z;
         }
         public Vector(int x, int y, int z)
         {
-            FstValue = x;
-            SndValue = y;
-            ThrdValue = z;
+            FirstValue = x;
+            SecondValue = y;
+            ThirdValue = z;
         }
 
-        public double FstValue
+        public double FirstValue
         {
-            get { return _fstValue; }
-            set { _fstValue = value; }
+            get { return _firstValue; }
+            set { _firstValue = value; }
         }
-        public double SndValue
+        public double SecondValue
         {
-            get { return _sndValue; }
-            set { _sndValue = value; }
+            get { return _secondValue; }
+            set { _secondValue = value; }
         }
-        public double ThrdValue
+        public double ThirdValue
         {
-            get { return _thrdValue; }
-            set { _thrdValue = value; }
+            get { return _thirdValue; }
+            set { _thirdValue = value; }
         }
 
         public override bool Equals(object other)
@@ -50,64 +50,64 @@ namespace BusinessLogic
             Vector otherVector = (Vector)other;
             const double tolerance = 0.0001;
 
-            bool evalFst = Math.Abs(FstValue - otherVector.FstValue) < tolerance;
-            bool evalSnd = Math.Abs(SndValue - otherVector.SndValue) < tolerance;
-            bool evalThrd = Math.Abs(ThrdValue - otherVector.ThrdValue) < tolerance;
+            bool evalFst = Math.Abs(FirstValue - otherVector.FirstValue) < tolerance;
+            bool evalSnd = Math.Abs(SecondValue - otherVector.SecondValue) < tolerance;
+            bool evalThrd = Math.Abs(ThirdValue - otherVector.ThirdValue) < tolerance;
 
             return evalFst && evalSnd && evalThrd;
         }
 
         public Vector Add(Vector other)
         {
-            return new Vector(_fstValue + other.FstValue, _sndValue + other.SndValue, _thrdValue + other.ThrdValue);
+            return new Vector(_firstValue + other.FirstValue, _secondValue + other.SecondValue, _thirdValue + other.ThirdValue);
         }
 
         public Vector Subtract(Vector other)
         {
-            return new Vector(_fstValue - other.FstValue, _sndValue - other.SndValue, _thrdValue - other.ThrdValue);
+            return new Vector(_firstValue - other.FirstValue, _secondValue - other.SecondValue, _thirdValue - other.ThirdValue);
         }
 
         public Vector Multiply(double value)
         {
-            return new Vector(_fstValue * value, _sndValue * value, _thrdValue * value);
+            return new Vector(_firstValue * value, _secondValue * value, _thirdValue * value);
         }
 
         public Vector Divide(double value)
         {
-            return new Vector(_fstValue / value, _sndValue / value, _thrdValue / value);
+            return new Vector(_firstValue / value, _secondValue / value, _thirdValue / value);
         }
 
         public void AddTo(Vector other)
         {
-            _fstValue += other.FstValue;
-            _sndValue += other.SndValue;
-            _thrdValue += other.ThrdValue;
+            _firstValue += other.FirstValue;
+            _secondValue += other.SecondValue;
+            _thirdValue += other.ThirdValue;
         }
 
         public void SubtractFrom(Vector other)
         {
-            _fstValue -= other.FstValue;
-            _sndValue -= other.SndValue;
-            _thrdValue -= other.ThrdValue;
+            _firstValue -= other.FirstValue;
+            _secondValue -= other.SecondValue;
+            _thirdValue -= other.ThirdValue;
         }
 
         public void ScaleUpBy(int iCount)
         {
-            _fstValue *= iCount;
-            _sndValue *= iCount;
-            _thrdValue *= iCount;
+            _firstValue *= iCount;
+            _secondValue *= iCount;
+            _thirdValue *= iCount;
         }
 
         public void ScaleDownBy(int iCount)
         {
-            _fstValue /= iCount;
-            _sndValue /= iCount;
-            _thrdValue /= iCount;
+            _firstValue /= iCount;
+            _secondValue /= iCount;
+            _thirdValue /= iCount;
         }
 
         public double SquaredLength()
         {
-            return _fstValue * _fstValue + _sndValue * _sndValue + _thrdValue * _thrdValue;
+            return _firstValue * _firstValue + _secondValue * _secondValue + _thirdValue * _thirdValue;
         }
 
         public double Length()
@@ -121,14 +121,14 @@ namespace BusinessLogic
 
         public double Dot(Vector other)
         {
-            return _fstValue * other.FstValue + _sndValue * other.SndValue + _thrdValue * other.ThrdValue;
+            return _firstValue * other.FirstValue + _secondValue * other.SecondValue + _thirdValue * other.ThirdValue;
         }
 
         public Vector Cross(Vector other)
         {
-            double x = _sndValue * other.ThrdValue - _thrdValue * other.SndValue;
-            double y = _thrdValue * other.FstValue - _fstValue * other.ThrdValue;
-            double z = _fstValue * other.SndValue - _sndValue * other.FstValue;
+            double x = _secondValue * other.ThirdValue - _thirdValue * other.SecondValue;
+            double y = _thirdValue * other.FirstValue - _firstValue * other.ThirdValue;
+            double z = _firstValue * other.SecondValue - _secondValue * other.FirstValue;
             return new Vector(x, y, z);
         }
     }
---
+++ BusinessLogic_Tests/RenderUnitTest.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace BusinessLogic_Tests
+{
+    [TestClass]
+    public class RenderUnitTest
+
+    {
+        [TestMethod]
+        public void TestRenderImage()
+        {
+            private Scene testScene = new Scene();
+            defaultLookFromVector = new Vector(0, 2, 0);
+            defaultLookAtVector = new Vector(0, 2, 5);
+            defaultFOV = 30;
+
+            testScene = new Scene()
+            {
+                Name = testName,
+                LookFrom = defaultLookFromVector,
+                LookAt = defaultLookAtVector,
+                FOV = defaultFOV
+            };
+
+            
+            sphereName = "Small sized sphere";
+            radius = 5;
+            
+            color = new Color(133, 94, 66);
+
+            testSphere = new Sphere(sphereName, radius);
+            color = new Color(0.5215686274509804, 0,3686274509803922, 0,2588235294117647);
+            lambertianName = "Oak color";
+            testLambertian = new Lambertian(lambertianName, color);
+
+            modelName = "Wooden ball";
+            testModel = new Model()
+            {
+                Name = modelName,
+                ModelShape = testSphere,
+                ModelColor = testLambertian
+            };
+
+            testPosition = new Vector(0, 0, 0);
+            testPositionedModel = new PositionedModel()
+            {
+                PositionedModelModel = testModel,
+                PositionedModelPosition = testPosition
+            };
+
+            private Motor motor = new Motor();
+            private Resolution = {10,10};
+            private PPM ppm = motor.render(testScene);
+            private PPM expected = [];
+
+            Assert.IsEqual(expected,ppm);
+
+        }
+    }
+}
