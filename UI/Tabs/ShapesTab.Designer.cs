ea9f7be66d8c5f19f55f3c47bf22b646b4f73d99
Santiago <santiagosalinasoto@gmail.com>
2023-05-07 19:25:10 -0300

    Plus button activates button

--- UI/Tabs/ShapesTab.Designer.cs
+++ UI/Tabs/ShapesTab.Designer.cs
@@ -76,6 +76,7 @@
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 2;
             this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.addShapeButton_Click);
             // 
             // ShapesTab
             // 
