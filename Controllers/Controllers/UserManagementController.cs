b77efab68e5066551b74db99fde1b537cc645b2b
Fede PC <federicorodriguez2002@gmail.com>
2023-05-25 21:50:09 -0300

    Started login and signup service. TODO: Move validations for adding and removing objects from repos to service/controllers

---
+++ Controllers/Controllers/UserManagementController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Controllers.Controllers
+{
+    public class UserManagementController
+    {
+        private 
+        public UserManagementController() { }
+
+    }
+}
---
+++ Services/UserService.cs
@@ -0,0 +1,38 @@
+﻿using BusinessLogic;
+using Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services
+{
+    public class UserService
+    {
+         private readonly IUserRepository _userRepository;
+
+            public UserService(IUserRepository userRepository)
+            {
+                _userRepository = userRepository;
+            }
+
+            public void SignUp(string username, string password)
+            {
+                User newUser = new User
+                {
+                    UserName = username,
+                    Password = password
+                };
+
+                _userRepository.AddUser(newUser);
+            }
+
+            public bool Login(string username, string password)
+            {
+                bool isValidCombination = _userRepository.CheckUsernameAndPasswordCombination(username, password);
+
+                return isValidCombination;
+            }
+    }
+}
