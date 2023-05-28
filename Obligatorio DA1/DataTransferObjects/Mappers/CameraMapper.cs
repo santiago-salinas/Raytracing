02076590b6c7bd4202273afef9e652f2d67f6900
Fede PC <federicorodriguez2002@gmail.com>
2023-05-27 23:33:11 -0300

    Added new mappers

---
+++ Obligatorio DA1/DataTransferObjects/Mappers/CameraMapper.cs
@@ -0,0 +1,46 @@
+﻿using BusinessLogic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataTransferObjects
+{
+    public static class CameraMapper
+    {
+        public static UICameraDTO ConvertToUICamDTO(BLCameraDTO providedDTO)
+        {
+            UICameraDTO uiCamera = new UICameraDTO()
+            {
+                FieldOfView = providedDTO.FieldOfView,
+                LookAt = VectorMapper.ConvertToDTO(providedDTO.LookAt),
+                LookFrom = VectorMapper.ConvertToDTO(providedDTO.LookFrom),
+                MaxDepth = providedDTO.MaxDepth,
+                ResolutionX = providedDTO.ResolutionX,
+                ResolutionY = providedDTO.ResolutionY,
+                SamplesPerPixel = providedDTO.SamplesPerPixel,
+                Up = VectorMapper.ConvertToDTO(providedDTO.Up)                
+            };
+
+            return uiCamera;
+        }
+
+        public static BLCameraDTO ConvertToDomainCamDTO(UICameraDTO providedDTO)
+        {
+            BLCameraDTO domainDTO = new BLCameraDTO()
+            {
+                FieldOfView = providedDTO.FieldOfView,
+                LookAt = VectorMapper.ConvertToVector(providedDTO.LookAt),
+                LookFrom = VectorMapper.ConvertToVector(providedDTO.LookFrom),
+                MaxDepth = providedDTO.MaxDepth,
+                ResolutionX = providedDTO.ResolutionX,
+                ResolutionY = providedDTO.ResolutionY,
+                SamplesPerPixel = providedDTO.SamplesPerPixel,
+                Up = VectorMapper.ConvertToVector(providedDTO.Up)
+            };
+
+            return domainDTO;
+        }
+    }
+}
---
+++ Obligatorio DA1/DataTransferObjects/Mappers/VectorMapper.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BusinessLogic;
+
+namespace DataTransferObjects
+{
+    public static class VectorMapper
+    {
+        public static VectorDTO ConvertToDTO(Vector vector)
+        {
+            VectorDTO vectorDTO = new VectorDTO()
+            {
+                FirstValue = vector.FirstValue,
+                SecondValue = vector.SecondValue,
+                ThirdValue  = vector.ThirdValue,
+            };
+
+            return vectorDTO;
+        }
+
+        public static Vector ConvertToVector(VectorDTO dto) 
+        {
+            Vector vector = new Vector()
+            {
+                FirstValue = dto.FirstValue,
+                SecondValue = dto.SecondValue,
+                ThirdValue = dto.ThirdValue,
+            };
+
+            return vector;
+        }
+    }
+}
