07b3a71703b0e0bddaa5ee67cb3dfb11dcaf9b32
Santiago <santiagosalinasoto@gmail.com>
2023-06-11 17:25:18 -0300

    Entity for Scenes

---
+++ DataAccess/Entities/CameraEntity.cs
@@ -0,0 +1,60 @@
+﻿using BusinessLogic;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Entities
+{
+    public class CameraEntity
+    {
+        [Key]
+        public Guid Id { get; set; }
+        public Vector LookFrom { get; set; }
+        public Vector LookAt { get; set; }
+        public Vector Up { get; set; }
+        public int FieldOfView { get; set; }
+        public int ResolutionX { get; set; }
+        public int ResolutionY { get; set; }
+        public int SamplesPerPixel { get; set; }
+        public int MaxDepth { get; set; }
+        public double Aperture { get; set; }
+
+        public static BLCameraDTO FromEntity(CameraEntity entity)
+        {
+            BLCameraDTO ret = new BLCameraDTO()
+            {
+                LookFrom = entity.LookFrom,
+                LookAt = entity.LookAt,
+                Up = entity.Up,
+                FieldOfView = entity.FieldOfView,
+                ResolutionX = entity.ResolutionX,
+                ResolutionY = entity.ResolutionY,
+                SamplesPerPixel = entity.SamplesPerPixel,
+                MaxDepth = entity.MaxDepth,
+                Aperture = entity.Aperture
+            };
+            return ret;
+        }
+
+        public static CameraEntity FromDomain(BLCameraDTO camera)
+        {
+            return new CameraEntity()
+            {
+                Id = Guid.NewGuid(),
+                LookFrom = camera.LookFrom,
+                LookAt = camera.LookAt,
+                Up = camera.Up,
+                FieldOfView = camera.FieldOfView,
+                ResolutionX = camera.ResolutionX,
+                ResolutionY = camera.ResolutionY,
+                SamplesPerPixel = camera.SamplesPerPixel,
+                MaxDepth = camera.MaxDepth,
+                Aperture = camera.Aperture
+            };
+        }
+    }
+}
--- DataAccess/Entities/SceneEntity.cs
+++ DataAccess/Entities/SceneEntity.cs
@@ -6,6 +6,7 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Runtime.InteropServices;
 
 namespace DataAccess.Entities
 {
@@ -23,18 +24,71 @@ namespace DataAccess.Entities
         public DateTime LastModificationDate { get; set; }
         public DateTime LastRenderDate { get; set; }
         // public PPM Preview { get; set; }
-        public List<PositionedModel> PositionedModels { get; set; }
+        public ICollection<PositionedModelEntity> PositionedModels { get; set; }
         public BLCameraDTO CameraDTO { get; set; }
         public bool Blur { get; set; }
 
-        public static Scene FromEntity(ModelEntity entity)
+        public static Scene FromEntity(SceneEntity entity)
         {
-            throw new NotImplementedException();
+            List<PositionedModel> posModels = new List<PositionedModel>();
+
+            foreach(PositionedModelEntity elem in entity.PositionedModels)
+            {
+                PositionedModel newPosModel = new PositionedModel()
+                {
+                    Model = ModelEntity.FromEntity(elem.Model),
+                    Position = new Vector(elem.PositionX, elem.PositionY, elem.PositionZ),
+                };
+
+                posModels.Add(newPosModel);
+            }
+
+            Scene ret = new Scene()
+            {
+                Name = entity.Name,
+                Owner = entity.OwnerId,
+                CreationDate = entity.CreationDate,
+                LastModificationDate = entity.LastModificationDate,
+                LastRenderDate = entity.LastRenderDate,
+                CameraDTO = entity.CameraDTO,
+                Blur = entity.Blur,
+                PositionedModels = posModels
+            };
+            return ret;
         }
 
-        public static SceneEntity FromDomain(Model model, EFContext efContext)
+        public static SceneEntity FromDomain(Scene scene, EFContext efContext)
         {
-            throw new NotImplementedException();
+            ICollection<PositionedModelEntity> posModelsEntities = new List<PositionedModelEntity>();
+
+            foreach (PositionedModel elem in scene.PositionedModels)
+            {
+                Vector position = elem.Position;
+                PositionedModelEntity newPosModelEntity = new PositionedModelEntity()
+                {
+                    Id = Guid.NewGuid(),
+                    Model = ModelEntity.FromDomain(elem.Model, efContext),
+                    PositionX = position.FirstValue,
+                    PositionY = position.SecondValue,
+                    PositionZ = position.ThirdValue,
+
+                };
+
+                posModelsEntities.Add(newPosModelEntity);
+            }
+
+            SceneEntity ret = new SceneEntity() { 
+                Name = scene.Name,
+                OwnerId = scene.Owner,
+                CreationDate = scene.CreationDate,
+                LastModificationDate = scene.LastModificationDate,
+                LastRenderDate = scene.LastRenderDate,
+                CameraDTO = scene.CameraDTO,
+                Blur = scene.Blur,
+                PositionedModels = posModelsEntities
+            };
+
+            return ret;
         }
     }
 }
