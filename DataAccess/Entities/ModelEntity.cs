229eeaf7ed7bc4e9af9be4130bc39c23529c2a34
Fede PC <federicorodriguez2002@gmail.com>
2023-06-11 13:04:39 -0300

    Conversion methods for modelEntity, missing PPM

--- DataAccess/Entities/ModelEntity.cs
+++ DataAccess/Entities/ModelEntity.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using DataAccess.Entities;
+using BusinessLogic;
 
 namespace DataAccess
 {
@@ -21,5 +22,41 @@ namespace DataAccess
         public UserEntity Owner { get; set; }
         public MaterialEntity Material { get; set; }
         public SphereEntity Shape { get; set; }
+
+        public static Model FromEntity(ModelEntity entity)
+        {
+            Material material = MaterialEntity.FromEntity(entity.Material);
+            Sphere shape = SphereEntity.FromEntity(entity.Shape);
+
+            Model ret = new Model()
+            {
+                Name = entity.Name,
+                Owner = entity.OwnerId,
+                Material = material,
+                Shape = shape,
+            };
+
+            return ret;
+        }
+
+        public static ModelEntity FromDomain(Model model, EFContext efContext)
+        {
+            UserEntity userEntity = efContext.UserEntities.FirstOrDefault(u => u.Username == model.Owner);
+
+            string materialName = model.Material.Name;
+            string shapeName = model.Shape.Name;
+            MaterialEntity materialEntity = efContext.MaterialEntities.FirstOrDefault(m => m.Owner == userEntity && m.Name == materialName);
+            SphereEntity sphereEntity = efContext.SphereEntities.FirstOrDefault(s => s.Owner == userEntity && s.Name == shapeName);
+
+            ModelEntity ret = new ModelEntity()
+            {
+                Name = model.Name,
+                Owner = userEntity,
+                Material = materialEntity,
+                Shape = sphereEntity,
+            };
+
+            return ret;
+        }
     }
 }
